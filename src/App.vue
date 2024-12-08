d748babe03791af320ceae73a4e4658d261e7ec0
alexchexes <alexchexes@gmail.com>
2024-12-08 21:05:10 +0000

    chore(App.vue): add suggestion display and styling

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
 import VueDadata from './VueDadata.vue';
-import type { HighlightOptions } from './types';
+import type { HighlightOptions, Suggestion } from './types';
 
 const token = import.meta.env.VITE_APP_DADATA_API_KEY as string;
 
 const query = ref('');
-const suggestion = ref(undefined);
+const suggestion = ref<Suggestion | undefined>(undefined);
 const highlightOptions: HighlightOptions = {
   highlightTag: 'span',
+};
+
+const reset = () => {
+  query.value = '';
+  suggestion.value = undefined;
 };
 </script>
 
@@ -21,6 +26,13 @@ const highlightOptions: HighlightOptions = {
       :selectOnBlur="true"
       :token="token"
     />
+
+    <section v-if="suggestion">
+      Current suggestion:
+      <button @click="reset">Clean</button>
+
+      <pre>{{ suggestion }}</pre>
+    </section>
   </main>
 </template>
 
@@ -28,11 +40,25 @@ const highlightOptions: HighlightOptions = {
 body {
   margin: 0;
   font-family: ui-sans-serif, system-ui, sans-serif;
+  background-color: #e9ebef;
+  font-size: 16px;
 }
 
 main {
-  max-width: 600px;
+  max-width: 700px;
   margin: 0 auto;
-  padding: 96px 16px 200px;
+  padding: 48px 16px 200px;
+}
+
+section {
+  margin-top: 10px;
+  border-radius: 4px;
+  background-color: #fff;
+  padding: 10px 20px;
+}
+
+pre {
+  white-space: pre-wrap;
+  font-size: 14px;
 }
 </style>
